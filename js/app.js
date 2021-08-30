4de148c19e8fa20771313fe847334f6271e87b06
Sakou <86302989+Sakou123@users.noreply.github.com>
2021-08-30 10:35:53 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,88 @@
-// import { user } from "./user.js";
+let operationsCompte = [];
+operationsCompte.push(["+", "salaire", 1520]);
+operationsCompte.push(["-", "achat PS4", 499.99]);
+operationsCompte.push(["-", "achat TV", 599]);
 
-// console.dir(user);
+let OperationsDebit = []
+OperationsDebit.push(["achat PS4", 499.99])
+OperationsDebit.push(["achat TV", 599])
 
-// console.log(
-//   `hello ${user.firstname}, i know your password :-) ${user.password}`
-// );
-
-// document.querySelector("#firstname").textContent = user.firstname;
-
-// on stocke toutes les opérations de compte dans un array[]
-let operationsCompte = [
-  // exemples
-  // ["+", "salaire", 1520],
-  // ["-", "achat PS4", 499.99],
-  // ["-", "achat TV", 599],
-];
 // on déclare nos variables
 let solde = 0;
 let operator = "";
 let devise = "€";
 
-function calcul(operateur, libelle, montant) {}
-// on execute la function
-// calcul();
+let totalAccount = document.getElementById('total');
+
+let credit = document.getElementById('detailsCredit')
+let debit = document.getElementById('detailsDebit')
+let totalCredit = document.getElementById('totalCredit')
+let totalDebit = document.getElementById('totalDebit')
+
+let audio = new Audio('/sound/stonks.mp3');
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 // send form, add operation
 const formulaire = document.getElementById("ajoutOperation");
 formulaire.addEventListener("submit", function (e) {
     e.preventDefault();
     // on récupère les valeurs des champs du formulaire
-    let result = document.getElementById('operation')
-    let textForm = document.getElementById('intitule').textContent.value
-    let montant = document.getElementById('montant').textContent.value
+    let operateur = document.getElementById('operation').value
+    let libelle = document.getElementById('intitule').value
+    let montant = parseFloat(document.getElementById('montant').value)
     // on stocke ces valeurs dans un array[]
-    let arrayOperations = [credit,debit,textForm,montant]
+    let arrayOperations = [operateur,libelle,montant]
+
     // on ajoute cet array dans notre array global operationsCompte
-    operationsCompte += arrayOperations
-    // on execute la fonction pour actualiser
+    operationsCompte.push(arrayOperations);
+
+    let creditTot = 0
+    let debitTot = 0
+    
+    operationsCompte.forEach(element => {
+      if(element[0]=='+'){
+        creditTot = creditTot + element[2];
+      } else {
+        debitTot = debitTot + element[2];
+      }
+    });
+
+    if (operateur == '+'){
+      let NewLi = document.createElement('li');
+      NewLi.innerHTML = "<span class='intitule'>"+capitalize(libelle)+"</span><span class='montant txt-color-gazoil'>"+montant+" "+devise+"</span>";
+      credit.appendChild(NewLi);
+      // if (montant>1000){                           /* STONKS */
+      //   audio.play();
+      // }
+    } 
+    if (operateur == '-'){
+      OperationsDebit.push([libelle, montant]);
+      console.log(OperationsDebit);
+      debit.innerHTML = '';
+      OperationsDebit.forEach(element => {
+        let NewLi = document.createElement('li');
+        NewLi.innerHTML = "<span class='intitule'>"+capitalize(element[0])+"</span><span class='montant txt-color-red'>"+element[1]+" "+devise+"</span><span class='percent txt-color-red'>"+Math.round(element[1]/debitTot*10000)/100+"%</span>";
+        debit.appendChild(NewLi);
+      });
+    }
+  let AccountTot = creditTot-debitTot
+
+  totalCredit.innerHTML = " "+creditTot+" "+devise;
+  totalDebit.innerHTML = " "+debitTot+" "+devise+"<span class='percent txt-color-red'>"+(debitTot/creditTot*100).toFixed(2)+"%</span>";
+
+  totalAccount.innerHTML = " "+Math.round(AccountTot*100)/100+" "+devise;       // Round AccountTot
+
+  if (AccountTot<0){
+    totalAccount.style.color = "red";
+    totalAccount.style.fontSize = "3.5rem"
+    totalAccount.innerHTML = "⚠ "+Math.round(AccountTot*100)/100+" "+devise;
+  } else {
+    totalAccount.style.color = "white";
+    totalAccount.style.fontSize = "2.5rem"
+  }
 
     // on reset le formulaire
     formulaire.reset();
